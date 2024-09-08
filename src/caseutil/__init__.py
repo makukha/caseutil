fefbb7b9dda53a87cc1c0c0ec2cec786db7138c0
Michael Makukha <m.makukha@gmail.com>
2024-09-09 00:19:47 +0300

    Fix formatting and types

--- src/caseutil/__init__.py
+++ src/caseutil/__init__.py
@@ -1,6 +1,7 @@
 """
 Case convert and verify for Python: snake_case, camelCase, kebab-case, and more.
 """
+
 __version__ = '0.6.4'
 
 from argparse import ArgumentParser
@@ -9,27 +10,42 @@ import re
 import sys
 
 __all__ = [
-    'is_camel', 'to_camel',
-    'is_const', 'to_const',
-    'is_kebab', 'to_kebab',
-    'is_lower', 'to_lower',
-    'is_pascal', 'to_pascal',
-    'is_snake', 'to_snake',
-    'is_title', 'to_title',
-    'is_upper', 'to_upper',
+    'is_camel',
+    'is_const',
+    'is_kebab',
+    'is_lower',
+    'is_pascal',
+    'is_snake',
+    'is_title',
+    'is_upper',
+    'to_camel',
+    'to_const',
+    'to_kebab',
+    'to_lower',
+    'to_pascal',
+    'to_snake',
+    'to_title',
+    'to_upper',
     # universal
-    'is_case', 'to_case',
-    'Case', 'words',
+    'Case',
+    'is_case',
+    'to_case',
+    'words',
 ]
 
 
 if sys.version_info.major == 2:  # pragma: no cover
-    parents = tuple()  # pragma: no cover
+
+    class _Case:  # pragma: no cover
+        pass  # pragma: no cover
 else:
     from enum import Enum
-    parents = (str, Enum)
 
-class Case(*parents):
+    class _Case(str, Enum):  # type: ignore[no-redef]
+        pass
+
+
+class Case(_Case):
     CAMEL = 'camel'
     CONST = 'const'
     KEBAB = 'kebab'
@@ -41,9 +57,10 @@ class Case(*parents):
 
 
 if sys.version_info.major == 2:  # pragma: no cover
-    CASES = tuple(v for k, v in Case.__dict__.items() if not k.startswith('_'))  # pragma: no cover
+    keys = [k for k in Case.__dict__ if not k.startswith('_')]  # pragma: no cover
+    CASES = tuple(Case.__dict__[k] for k in keys)  # pragma: no cover
 else:
-    CASES = tuple(c.value for c in Case)
+    CASES = tuple(c.value for c in Case.__members__)  # type: ignore[attr-defined]
 
 
 # case patterns
@@ -68,11 +85,13 @@ RX_SIMPLE_SEP = re.compile(r'(_|\W)+')
 RX_CASE_SEP1 = re.compile(r'(?P<pre>[a-z][0-9]*)(?P<post>[A-Z])')
 RX_CASE_SEP2 = re.compile(r'(?P<pre>[A-Z][0-9]*)(?P<post>[A-Z][0-9]*[a-z])')
 
+
 def tokenize(text: str) -> str:
     values = RX_SIMPLE_SEP.sub(',', text)
     values = RX_CASE_SEP1.sub(r'\g<pre>,\g<post>', values)
     values = RX_CASE_SEP2.sub(r'\g<pre>,\g<post>', values)
     return values.strip(',')
+
 
 def words(text: str) -> list[str]:
     return tokenize(text).split(',')
@@ -80,65 +99,88 @@ def words(text: str) -> list[str]:
 
 # const case
 
+
 def is_const(text: str) -> bool:
     return True if RX_CONST.fullmatch(text) else False
+
 
 def to_const(text: str) -> str:
     return tokenize(text).upper().replace(',', '_')
 
+
 # camel case
+
 
 def is_camel(text: str) -> bool:
     return True if RX_CAMEL.fullmatch(text) else False
+
 
 def to_camel(text: str) -> str:
     wrds = words(text)
     return ''.join([wrds[0].lower(), *(w.title() for w in wrds[1:])])
 
+
 # kebab case
+
 
 def is_kebab(text: str) -> bool:
     return True if RX_KEBAB.fullmatch(text) else False
 
+
 def to_kebab(text: str) -> str:
     return tokenize(text).lower().replace(',', '-')
 
+
 # lower case
+
 
 def is_lower(text: str) -> bool:
     return True if RX_LOWER.fullmatch(text) else False
 
+
 def to_lower(text: str) -> str:
     return tokenize(text).lower().replace(',', ' ')
 
+
 # pascal case
+
 
 def is_pascal(text: str) -> bool:
     return True if RX_PASCAL.fullmatch(text) else False
 
+
 def to_pascal(text: str) -> str:
     return ''.join(w.title() for w in words(text))
 
+
 # snake case
+
 
 def is_snake(text: str) -> bool:
     return True if RX_SNAKE.fullmatch(text) else False
 
+
 def to_snake(text: str) -> str:
     return tokenize(text).lower().replace(',', '_')
 
+
 # title case
+
 
 def is_title(text: str) -> bool:
     return True if RX_TITLE.fullmatch(text) else False
 
+
 def to_title(text: str) -> str:
     return ' '.join(w.title() for w in words(text))
 
+
 # upper case
+
 
 def is_upper(text: str) -> bool:
     return True if RX_UPPER.fullmatch(text) else False
+
 
 def to_upper(text: str) -> str:
     return tokenize(text).upper().replace(',', ' ')
@@ -146,28 +188,37 @@ def to_upper(text: str) -> str:
 
 # universal functions
 
+
 def is_case(case: Case | str, text: str) -> bool:
-    if case == Case.CAMEL: return is_camel(text)  # noqa: E701
-    elif case == Case.CONST: return is_const(text)  # noqa: E701
-    elif case == Case.KEBAB: return is_kebab(text)  # noqa: E701
-    elif case == Case.LOWER: return is_lower(text)  # noqa: E701
-    elif case == Case.PASCAL: return is_pascal(text)  # noqa: E701
-    elif case == Case.SNAKE: return is_snake(text)  # noqa: E701
-    elif case == Case.TITLE: return is_title(text)  # noqa: E701
-    elif case == Case.UPPER: return is_upper(text)  # noqa: E701
-    else: raise ValueError(f'Unsupported case: {case}')  # noqa: E701
+    try:
+        return {
+            'camel': is_camel,
+            'const': is_const,
+            'kebab': is_kebab,
+            'lower': is_lower,
+            'pascal': is_pascal,
+            'snake': is_snake,
+            'title': is_title,
+            'upper': is_upper,
+        }[str(case)](text)
+    except IndexError:
+        raise ValueError(f'Unsupported case: {case}')
 
 
 def to_case(case: Case | str, text: str) -> str:
-    if case == Case.CAMEL: return to_camel(text)  # noqa: E701
-    elif case == Case.CONST: return to_const(text)  # noqa: E701
-    elif case == Case.KEBAB: return to_kebab(text)  # noqa: E701
-    elif case == Case.LOWER: return to_lower(text)  # noqa: E701
-    elif case == Case.PASCAL: return to_pascal(text)  # noqa: E701
-    elif case == Case.SNAKE: return to_snake(text)  # noqa: E701
-    elif case == Case.TITLE: return to_title(text)  # noqa: E701
-    elif case == Case.UPPER: return to_upper(text)  # noqa: E701
-    else: raise ValueError(f'Unsupported case: {case}')  # noqa: E701
+    try:
+        return {
+            'camel': to_camel,
+            'const': to_const,
+            'kebab': to_kebab,
+            'lower': to_lower,
+            'pascal': to_pascal,
+            'snake': to_snake,
+            'title': to_title,
+            'upper': to_upper,
+        }[str(case)](text)
+    except IndexError:
+        raise ValueError(f'Unsupported case: {case}')
 
 
 # cli
@@ -187,7 +238,3 @@ def main() -> None:
     )
     values = [to_case(args.c, line) for line in lines]
     print(*values, sep='\n')
-
-
-if __name__ == '__main__':
-    main()
